e6950be2c9e18a4c33daf4c15d5e88d71d02f39b
cookie1129gu <994032671@qq.com>
2022-09-06 15:30:34 +0800

    Cookie 4

--- ABABProject/src/main/java/com/abab/util/ConstUtil.java
+++ ABABProject/src/main/java/com/abab/util/ConstUtil.java
@@ -5,6 +5,24 @@ public class ConstUtil {
     public static String ADMIN = "ADMIN";
     public static String STAFF = "STAFF";
     public static String VIDEO = "VIDEO";
+
+    public static Integer GINDER_MALE = 1;
+    public static Integer GINDER_FELMALE = 2;
+
+    public static Integer ADMIN_ROLE = 1;//审核员
+    public static Integer STAFF_ROLE = 2;//管理员
+    public static Integer VIP_ROLE = 3;//大会员
+    public static Integer USER_ROLE = 4;//普通用户
+
+    //日志操作
+    public static Integer LOG_ADD=1;
+    public static Integer LOG_UPDETE=2;
+    public static Integer LOG_DEL=3;
+
+
+    public static String UNROLE="无权限";
+    public static String RELOGIN="已登录";
+
     public static String USER_UNLOGIN = "用户未登录";
     public static String ADMIN_UNLOGIN = "管理员未登录";
     public static String STAFF_UNLOGIN = "员工未登录";
@@ -13,6 +31,10 @@ public class ConstUtil {
     public static String WRONG_PASSWORD = "错误的密码";
     public static String NOTALLOW_EMPTY = "不允许为空";
     public static String OVERLIMITED_LENGTH = "长度超限";
+
+    public static String DATA_UNEXIST = "数据不存在";
+
+
     public static String UNROLE="无权限";
     public static String RELOGIN="已登录";
     public static Integer ADMIN_ROLE=2;
