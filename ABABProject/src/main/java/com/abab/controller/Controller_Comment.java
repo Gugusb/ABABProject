a1bebdb5d58f24996cb1434a7433a51532a4ebbc
cookie1129gu <994032671@qq.com>
2022-09-06 15:30:34 +0800

    Cookie 4

--- ABABProject/src/main/java/com/abab/controller/Controller_Comment.java
+++ ABABProject/src/main/java/com/abab/controller/Controller_Comment.java
@@ -1,8 +1,15 @@
 package com.abab.controller;
 
 import com.abab.common.ServerResponse;
+import com.abab.entity.BiliUser;
 import com.abab.entity.BiliVideo;
 import com.abab.entity.BiliComment;
+import com.abab.service.CommentService;
+import com.abab.util.ConstUtil;
+import com.abab.util.EmptyJudger;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,22 +20,122 @@ import java.util.List;
 
 @RestController
 public class Controller_Comment {
+    //发评论的是用户，查删评论的是管理员
+
+    @Autowired
+    CommentService commentService;
+
+    public ServerResponse<List<BiliComment>> getCommentsByVideoIdService(BiliVideo biliVideo){
+        ServerResponse<List<BiliComment>> serverResponse = null;
+
+        List<BiliComment> biliCommentList=null;
+
+        QueryWrapper qe = new QueryWrapper();
+        qe.eq("videoid" , biliVideo.getVideoid());
+        biliCommentList=commentService.list(qe);
+
+        if(!EmptyJudger.isEmpty(biliCommentList)){
+            serverResponse = ServerResponse.createRespBySuccess(biliCommentList);
+        }
+        else{
+            serverResponse = ServerResponse.createByErrorMessage(ConstUtil.DATA_UNEXIST);
+        }
+
+        return serverResponse;
+    }
+
+    public ServerResponse<BiliComment> postCommentService(BiliComment biliComment){
+        ServerResponse<BiliComment> serverResponse = null;
+
+        if(EmptyJudger.isEmpty(biliComment.getUserid())){
+            serverResponse = ServerResponse.createByErrorMessage("用户ID" + ConstUtil.NOTALLOW_EMPTY);
+        }else if(EmptyJudger.isEmpty(biliComment.getContent())){
+            serverResponse = ServerResponse.createByErrorMessage("评论内容" + ConstUtil.NOTALLOW_EMPTY);
+        }else if(EmptyJudger.isEmpty(biliComment.getVideoid())) {
+            serverResponse = ServerResponse.createByErrorMessage("视频ID" + ConstUtil.NOTALLOW_EMPTY);
+        }else if(biliComment.getContent().length() > 200) {
+            serverResponse = ServerResponse.createByErrorMessage("评论" + ConstUtil.OVERLIMITED_LENGTH);
+        }else if(biliComment.getMemo().length() > 200){
+            serverResponse = ServerResponse.createByErrorMessage("评论备注" + ConstUtil.OVERLIMITED_LENGTH);
+        }else{
+            commentService.save(biliComment);
+
+            serverResponse = ServerResponse.createRespBySuccess();
+        }
+
+        return serverResponse;
+    }
+
+    public ServerResponse<BiliComment> deleteCommentService(BiliComment biliComment){
+        //按照评论id删除评论
+        ServerResponse<BiliComment> serverResponse = null;
+
+        if(!EmptyJudger.isEmpty(biliComment.getId())){
+            QueryWrapper qe = new QueryWrapper();
+            qe.eq("id",biliComment.getId());
+
+            commentService.remove(qe);
+
+            serverResponse = ServerResponse.createRespBySuccess();
+        }
+        else{
+            serverResponse = ServerResponse.createByErrorMessage(ConstUtil.STAFF_UNLOGIN);
+        }
+
+
+        return serverResponse;
+    }
+
+
 
     @RequestMapping(value = "/comment/getcommentsbyvideoid", method = RequestMethod.POST)
     public ServerResponse<List<BiliComment>> getCommentsByVideoId(HttpSession httpSession,
                                                                   BiliVideo biliVideo,
                                                                   @RequestParam(defaultValue = "1") Integer pageIndex,
                                                                   @RequestParam(defaultValue = "5") Integer pageSize){
-        return null;
+        ServerResponse<List<BiliComment>> serverResponse = null;
+
+        PageHelper.startPage(pageIndex, pageSize);
+
+        if(!EmptyJudger.isEmpty(httpSession.getAttribute(ConstUtil.STAFF))){
+            serverResponse = getCommentsByVideoIdService(biliVideo);
+        }
+        else{
+            serverResponse = ServerResponse.createByErrorMessage(ConstUtil.STAFF_UNLOGIN);
+        }
+
+        return serverResponse;
     }
 
     @RequestMapping(value = "/comment/postcomment", method = RequestMethod.POST)
     public ServerResponse<BiliComment> postComment(HttpSession httpSession, BiliComment biliComment){
-        return null;
+        ServerResponse<BiliComment> serverResponse = null;
+
+        if(!EmptyJudger.isEmpty(httpSession.getAttribute(ConstUtil.USER))){
+
+            //将用户id赋予评论对象
+            biliComment.setUserid(((BiliUser)httpSession.getAttribute(ConstUtil.USER)).getUserid());
+
+            serverResponse = postCommentService(biliComment);
+        }
+        else{
+            serverResponse = ServerResponse.createByErrorMessage(ConstUtil.USER_UNLOGIN);
+        }
+
+        return serverResponse;
     }
 
     @RequestMapping(value = "/comment/deletecomment", method = RequestMethod.POST)
     public ServerResponse<BiliComment> deleteComment(HttpSession httpSession, BiliComment biliComment){
-        return null;
+        ServerResponse<BiliComment> serverResponse = null;
+
+        if(!EmptyJudger.isEmpty(httpSession.getAttribute(ConstUtil.STAFF))){
+            serverResponse = deleteCommentService(biliComment);
+        }
+        else{
+            serverResponse = ServerResponse.createByErrorMessage(ConstUtil.STAFF_UNLOGIN);
+        }
+
+        return serverResponse;
     }
 }
--- ABABProject/src/main/java/com/abab/util/ConstUtil.java
+++ ABABProject/src/main/java/com/abab/util/ConstUtil.java
@@ -9,10 +9,10 @@ public class ConstUtil {
     public static Integer GINDER_MALE = 1;
     public static Integer GINDER_FELMALE = 2;
 
-    public static Integer ADMIN_ROLE_INDEX = 1;//审核员
-    public static Integer STAFF_ROLE_INDEX = 2;//管理员
-    public static Integer VIP_ROLE_INDEX = 3;//大会员
-    public static Integer USER_ROLE_INDEX = 4;//普通用户
+    public static Integer ADMIN_ROLE = 1;//审核员
+    public static Integer STAFF_ROLE = 2;//管理员
+    public static Integer VIP_ROLE = 3;//大会员
+    public static Integer USER_ROLE = 4;//普通用户
 
     //日志操作
     public static Integer LOG_ADD=1;
@@ -34,11 +34,14 @@ public class ConstUtil {
 
     public static String DATA_UNEXIST = "数据不存在";
 
-    public static String MEMO_USER_ROLE = "用户角色";
-    public static String MEMO_STAFF_ROLE = "员工角色";
-    public static String MEMO_AUDIT_STATE = "审核状态";
-    public static String MEMO_SHELF_STATE = "上架状态";
 
+    public static String UNROLE="无权限";
+    public static String RELOGIN="已登录";
+    public static Integer ADMIN_ROLE=2;
+    public static String USER_ROLE = "用户角色";
+    public static String STAFF_ROLE = "员工角色";
+    public static String AUDIT_STATE = "审核状态";
+    public static String SHELF_STATE = "上架状态";
     public static String WRONG_MEMO = "错误的Dictionary描述";
 
 }
