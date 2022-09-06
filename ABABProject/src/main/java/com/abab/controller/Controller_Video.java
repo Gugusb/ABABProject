152fdd9274c4b9a0886e1748be228766efd91278
Gugusb <931370347@qq.com>
2022-09-06 09:41:19 +0800

    Demo4.0

--- ABABProject/src/main/java/com/abab/controller/Controller_Video.java
+++ ABABProject/src/main/java/com/abab/controller/Controller_Video.java
@@ -4,20 +4,50 @@ import com.abab.common.ServerResponse;
 import com.abab.entity.BiliUser;
 import com.abab.entity.BiliVideo;
 import com.abab.entity.BiliDictionary;
+import com.abab.service.BiliVideoService;
+import com.abab.util.ConstUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.lang.constant.Constable;
 import java.util.List;
 
 @RestController
 public class Controller_Video {
 
+    @Autowired
+    private BiliVideoService biliVideoService;
+
+    @RequestMapping(value = "/video/openvideo", method = RequestMethod.POST)
+    public ServerResponse<BiliVideo> openVideo(HttpSession httpSession, BiliVideo biliVideo){
+        return null;
+    }
+
+    private ServerResponse<BiliVideo> getVideoInfoByIdService(BiliVideo biliVideo){
+        //判断id是否为空
+        if(biliVideo.getVideoid() == null){
+            return ServerResponse.createByErrorMessage(ConstUtil.VIDEO_UNEXIST);
+        }
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("videoid", biliVideo.getVideoid());
+        BiliVideo video = biliVideoService.getById(biliVideo);
+
+        if(video == null){
+            return ServerResponse.createByErrorMessage(ConstUtil.VIDEO_UNEXIST);
+        }else{
+            return ServerResponse.createRespBySuccess(video);
+        }
+    }
+
     @RequestMapping(value = "/video/getvideobyid", method = RequestMethod.POST)
     public ServerResponse<BiliVideo> getVideoInfoById(HttpSession httpSession, BiliVideo biliVideo){
-        return null;
+        ServerResponse<BiliVideo> serverResponse = getVideoInfoByIdService(biliVideo);
+        return serverResponse;
     }
 
     @RequestMapping(value = "/video/updatevideoinfo", method = RequestMethod.POST)
--- ABABProject/src/main/java/com/abab/util/ConstUtil.java
+++ ABABProject/src/main/java/com/abab/util/ConstUtil.java
@@ -9,6 +9,7 @@ public class ConstUtil {
     public static String ADMIN_UNLOGIN = "管理员未登录";
     public static String STAFF_UNLOGIN = "员工未登录";
     public static String USER_UNEXIST = "用户不存在";
+    public static String VIDEO_UNEXIST = "视频不存在";
     public static String WRONG_PASSWORD = "错误的密码";
     public static String NOTALLOW_EMPTY = "不允许为空";
     public static String OVERLIMITED_LENGTH = "长度超限";
